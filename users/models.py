9d571cf633be58242d605762423be64d92ee264e
Tumise <oladele.alade@gmail.com>
2021-05-07 22:21:48 +0100

    added fields to user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
+import uuid
+
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    country_code = models.CharField(max_length=5)
