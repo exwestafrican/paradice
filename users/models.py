eb2c5302c7549f24ff737405463ef29f62524752
Tumise <oladele.alade@gmail.com>
2021-05-07 22:44:01 +0100

    added activity log to user model

--- users/models.py
+++ users/models.py
@@ -7,4 +7,23 @@ from django.db import models
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    country_code = models.CharField(max_length=5)
+    country_code = models.CharField(max_length=3)
+    number = models.CharField(max_length=10)
+
+    def phone_number(self):
+        return "{}{}".format(self.country_code, self.number)
+
+
+class ActivityLog(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="activity_logger",
+    )
+    action = models.CharField(max_length=500)
+    success = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.action
