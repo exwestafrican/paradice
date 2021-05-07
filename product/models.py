ce0e168aa6bbea89bd73f7d83f295cc8db4c56df
Tumise <oladele.alade@gmail.com>
2021-05-07 21:26:38 +0100

    added products to setting formatted settings

--- product/models.py
+++ product/models.py
@@ -1,23 +1,26 @@
 from django.db import models
 import uuid
+
 # Create your models here.
+
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.Charfield(max_length=500)
     price = models.DecimalField(max_digits=9, decimal_places=2)
-    units_available= models.PositiveSmallIntegerField()
+    units_available = models.PositiveSmallIntegerField()
     on_sale = models.BooleanField(default=False)
 
+
 class Image(models.Model):
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    img_variation= models.JSONField(default=dict())
-    
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    img_variation = models.JSONField(default=dict())
+
     def __str__(self):
         pass
 
     class Meta:
-        db_table = ''
+        db_table = ""
         managed = True
-        verbose_name = 'Image'
-        verbose_name_plural = 'Images'
+        verbose_name = "Image"
+        verbose_name_plural = "Images"
